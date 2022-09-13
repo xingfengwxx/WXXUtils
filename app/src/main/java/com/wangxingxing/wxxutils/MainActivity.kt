7ac3297b07755bb61ad5476fd9c1b0b245b7566b
WangXingxing <1099420259@qq.com>
2022-09-13 16:41:08 +0800

    Initial commit

---
+++ app/src/main/java/com/wangxingxing/wxxutils/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.wangxingxing.wxxutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * author : 王星星
+ * date : 2022/9/13 16:39
+ * email : 1099420259@qq.com
+ * description : 测试aar包上传到github
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
