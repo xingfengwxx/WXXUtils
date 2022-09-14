12bf68b205ca0d75a684fead58dc2fe3effde2c3
WangXingxing <1099420259@qq.com>
2022-09-14 14:28:37 +0800

    使用发布的aar

--- app/src/main/java/com/wangxingxing/wxxutils/MainActivity.kt
+++ app/src/main/java/com/wangxingxing/wxxutils/MainActivity.kt
@@ -2,6 +2,8 @@ package com.wangxingxing.wxxutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wangxingxing.mylibrary.DateUtil
+import com.wangxingxing.wxxutils.databinding.ActivityMainBinding
 
 /**
  * author : 王星星
@@ -10,8 +12,15 @@ import android.os.Bundle
  * description : 测试aar包上传到github
  */
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.tvContent.text = DateUtil.getDate(System.currentTimeMillis())
     }
 }
